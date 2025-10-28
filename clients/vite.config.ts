54f407f7ce3fb8a5f80fd5e2dc7e86e0abc86731
Ycaro Gonçalves <ycarogoncalves@gmail.com>
2025-10-28 20:38:28 -0300

    chore: :wrench: configure project remote clients
    configure project remote clients

--- clients/vite.config.ts
+++ clients/vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     federation({
       name: "remoteClients",
       filename: "remoteEntry.js",
@@ -16,6 +19,11 @@ export default defineConfig({
       shared: ["react", "react-dom"],
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   build: {
     modulePreload: false,
     target: "esnext",
