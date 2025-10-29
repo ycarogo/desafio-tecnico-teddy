181064208998f265b13a78b5784003d690a6aa62
Ycaro Gonçalves <ycarogoncalves@gmail.com>
2025-10-29 20:44:45 -0300

    feat: :art: created dockerfiles
    created dockerfiles

--- clients/src/main.tsx
+++ clients/src/main.tsx
@@ -1,5 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import Clients from "./pages/Clients";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Clients />
+  </StrictMode>
+);
