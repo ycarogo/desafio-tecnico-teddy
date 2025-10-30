79c27f6ed01c3e1512f75fbbce349cc134fb7a63
Ycaro Gonçalves <ycarogoncalves@gmail.com>
2025-10-30 10:46:14 -0300

    fix(host): ajustar eslint react-refresh e substituir any por unknown

--- host/src/types/remote-clients.d.ts
+++ host/src/types/remote-clients.d.ts
@@ -1,13 +1,11 @@
 declare module "remoteClients/Clients" {
   import type { ComponentType } from "react";
-  const Component: ComponentType<any>;
+  const Component: ComponentType<unknown>;
   export default Component;
 }
 
 declare module "remoteClients/ListClients" {
   import type { ComponentType } from "react";
-  const Component: ComponentType<any>;
+  const Component: ComponentType<unknown>;
   export default Component;
 }
-
-
